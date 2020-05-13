988e0e27558ab7c138422b7bf3b0d25229300a6a
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-05-13 17:38:00 -0500

    added more variables

--- luminosity/views.py
+++ luminosity/views.py
@@ -11,8 +11,11 @@ from django.views.generic import TemplateView
 def luminosity(request):
     if 'value' in request.GET:
         value = request.GET['value']
+        latitude = request.GET['latitude']
+        length = request.GET['length']
+        terrain = request.GET['terrain']
         if value:
-            args = {'type': 'number', 'value': value}
+            args = {'type': 'number', 'value': value, 'latitude': latitude, 'length': length, 'terrain': terrain}
             response = requests.post('http://127.0.0.1:8000/luminosity/', args)
             measure_json = response.json()
     response = requests.get('http://127.0.0.1:8000/luminosity/')
